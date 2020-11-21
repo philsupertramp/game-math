12057f1b8f07a1750d43232a56c40c7e14aee826
Philipp Zettl <philipp@godesteem.de>
2020-11-21 15:27:56 +0100

    add requested changes

--- include/math/ode.h
+++ include/math/ode.h
@@ -9,16 +9,13 @@ std::vector<float> linspace(float start, float end, unsigned long num_elements)
     result.resize(num_elements);
     float h   = (end - start) / (float)(num_elements - 1);
     result[0] = start;
-    for(size_t i = 1; i < num_elements; i++) { result[i] = result[i - 1] + h; }
+    for(size_t i = 1; i < num_elements; i++) { result[i] = i * h; }
     return result;
 }
 
 std::vector<std::vector<float>> zeros(size_t rows, size_t columns)
 {
-    std::vector<std::vector<float>> result;
-    result.resize(rows);
-    for(size_t i = 0; i < rows; i++) { result[i].resize(columns); }
-    return result;
+    return std::vector<std::vector<float>>(rows, std::vector<float>(columns, 0));
 }
 
 using ODE       = std::function<std::vector<float>(float, std::vector<float>)>;
