032e501ddf50169da9f459a3ca58fa6613c8d8a2
Philipp Zettl <philipp@godesteem.de>
2021-03-30 12:15:58 +0200

    drop codecov, no support for c++>11

--- build.sh
+++ build.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
-set -x;
-
-bool(){ return "$((!${#1}))"; }
+set -o;
 
 DIR_NAME=$1
 WITH_COVERAGE=1; [ "$2" == "-c" ] || WITH_COVERAGE=0;
 
-echo "With coverage: ${WITH_COVERAGE}"
-
 mkdir -p ${DIR_NAME}
 (
   cd ${DIR_NAME};
-  cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_BUILD_TYPE=Debug -DMATH_COVERAGE=1 -DMATH_EXTENSIONS=numerics,ds -G "CodeBlocks - Unix Makefiles" ..
+  cmake -DCMAKE_BUILD_TYPE=Debug -DMATH_SILENCE_WARNING=1 -DCMAKE_BUILD_TYPE=Debug -DMATH_COVERAGE=1 -DMATH_EXTENSIONS=numerics,ds -G "CodeBlocks - Unix Makefiles" ..
   cmake --build ${DIR_NAME};
   make -j 3 ;
   if [ ${WITH_COVERAGE:-0} == 1 ]; then
