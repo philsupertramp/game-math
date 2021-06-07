98b78ebed5b99292ae48b305b1a307d9f9749f3d
Philipp Zettl <philipp@godesteem.de>
2021-06-07 12:16:55 +0200

    more content

--- include/math/symb/Symbolic.cpp
+++ include/math/symb/Symbolic.cpp
@@ -1,15 +1,17 @@
 #include "Symbolic.h"
 
 std::regex GetRegex(MathNodeType type) {
-    static const std::regex symbol_regex("([-]?[A-Za-z_]{1}[A-Za-z0-9_]*)");
-    static const std::regex number_regex("([-]?[0-9]+)");
-    static const std::regex parentheses_regex("\\(([\\(\\)+\\-*\\/\\^A-Za-z\\s0-9]*)\\)");
+    static const std::regex symbol_regex("([A-Za-z_]{1}[A-Za-z0-9_]?)");
+    static const std::regex default_symbol_regex("([A-Za-z_]+)");
+    static const std::regex number_regex("([0-9]+)");
+    static const std::regex parentheses_regex("\\(([^\\)]*)\\)");
     static const std::regex operator_regex("([\\-+*/\\^]+)");
     static const std::regex any_regex("([\\x00-\\x7F]+)");
     switch(type) {
         case NodeType_Symbolic: return symbol_regex;
         case NodeType_Numeric: return number_regex;
         case NodeType_Parentheses: return parentheses_regex;
+        case NodeType_DefaultSymbol: return default_symbol_regex;
         case NodeType_Any: return any_regex;
         default: return operator_regex;
     }
@@ -33,21 +35,29 @@ std::shared_ptr<Operator> GenerateOperator(OperatorType type) {
             return std::make_shared<Operator>(
             "^", [](double a, double b) { return pow(a, b); }, OperatorPriority::OPClassDot);
         case TYPE_PARENTHESES_OPEN:
-            return std::make_shared<Operator>(
+        {
+            auto op = std::make_shared<Operator>(
             "(",
             []([[maybe_unused]] double a, [[maybe_unused]] double b) { return 0.0; },
             OperatorPriority::OPClassParentheses);
+            op->connectionType = NodeConnectionType::ConnectionType_None;
+            return op;
+        }
         case TYPE_PARENTHESES_CLOSE:
-            return std::make_shared<Operator>(
+        {
+            auto op = std::make_shared<Operator>(
             ")",
             []([[maybe_unused]] double a, [[maybe_unused]] double b) { return 0.0; },
             OperatorPriority::OPClassParentheses);
+            op->connectionType = NodeConnectionType::ConnectionType_None;
+            return op;
+        }
     }
     return std::make_shared<Operator>(
     "",
     []([[maybe_unused]] double a, [[maybe_unused]] double b) {
-        std::cerr << "Operator not found.\n" << std::flush;
-        return -1;
+      std::cerr << "Operator not found.\n" << std::flush;
+      return -1;
     },
     OperatorPriority::OPClassUnknown);
 }
